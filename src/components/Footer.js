48cae5680433ef49926ffc44db9867ae3d48b539
Vighnesh Rasal <vighneshrasal@gmail.com>
2025-01-21 10:49:02 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,13 @@ export const Footer = () => {
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-evenly">
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
           <li>
-            <a href="#" className="hover:underline me-4 md:me-6">Instagram</a>
+            <a href="https://www.bing.com/search?q=instagram&form=ANNTH1&refig=D3E8F518C548485B85D1B673894BEC3B&pc=U531&pq=insta&pqlth=5&assgl=9&sgcn=instagram&qs=HS&smvpcn=0&swbcn=10&sctcn=0&sc=10-5&sp=1&ghc=0&cvid=D3E8F518C548485B85D1B673894BEC3B&clckatsg=1&hsmssg=0" className="hover:underline me-4 md:me-6">Instagram</a>
           </li>
           <li>
-            <a href="#" className="hover:underline me-4 md:me-6">Facebook</a>
+            <a href="https://www.bing.com/search?q=facebook&filters=ufn%3a%22Facebook%22+sid%3a%224bc8f781-7083-d1a0-f781-9466e0eb62e7%22&asbe=SS&qs=MB&pq=face&sc=8-4&cvid=C36E9813C52F410D99A9FF091D824F16&FORM=QBRE&sp=1&ghc=1&lq=0" className="hover:underline me-4 md:me-6">Facebook</a>
           </li>
           <li>
-            <a href="mailto:perficient6262@gmail.com" className="hover:underline me-4 md:me-6">perficient6262@gmail.com</a>
+            <a href="mailto:gkenterprises0119@gmail.com" className="hover:underline me-4 md:me-6">gkenterprises0119@gmail.com</a>
           </li>
         </ul>
       </div>
